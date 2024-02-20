70d8e71158c8d1aa2eebb4cbd75c6273100ac20c
César Lucas Júnior <cesarlucasjunior93@gmail.com>
2024-02-20 11:46:47 -0300

    Removendo pasta do front, atualizando codigo e dependencias

--- src/main/java/com/stefanini/projeto/repository/AlunoRepository.java
+++ src/main/java/com/stefanini/projeto/repository/AlunoRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.stefanini.projeto.model.Aluno;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long>{
 
 	public List<Aluno> findByNomeContainingIgnoreCase(String nome);
--- src/main/java/com/stefanini/projeto/repository/MochilaRepository.java
+++ src/main/java/com/stefanini/projeto/repository/MochilaRepository.java
@@ -3,7 +3,9 @@ package com.stefanini.projeto.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.stefanini.projeto.model.Mochila;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MochilaRepository extends JpaRepository<Mochila, Long> {
 
 	public Long countByMarca(String marca);
